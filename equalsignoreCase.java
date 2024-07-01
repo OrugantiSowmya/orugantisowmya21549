a6fbed9366f6567b94f2789259e72cf709eeee64
OrugantiSowmya <orugantisowmya19@gmail.com>
2024-07-01 10:26:29 +0530

    Create equalsignoreCase.java

---
+++ equalsignoreCase.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class String
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String s=new Scanner(Systrm.in);
+String s1=s.next();
+String s2=s.next();
+if(s1.equalsignoreCase(s2))
+System.out.println("Same");
+else
+ System.out.println("Not Same");
+}
+}
