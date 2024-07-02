ca5b4399890682662dfc1afed5ab17b84c7d869e
OrugantiSowmya <orugantisowmya19@gmail.com>
2024-07-02 12:04:05 +0530

    Create String compare5.java
    Input (stdin)
    
    java
    java
    Your Output (stdout)
    
    Strings are same
    Expected Output
    
    Strings are same

---
+++ String compare5.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class StringComparator {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        String str1 = s.nextLine();
+        String str2 = s.nextLine();
+        if (str1.equals(str2)){
+            
+            System.out.println("Strings are same");
+        } 
+        else
+        {
+           System.out.println("Strings are not same"); 
+        }
+    }
+}
