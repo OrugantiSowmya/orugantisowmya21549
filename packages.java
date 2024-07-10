8747d16fa16de80088a66d7a097c011a472b819d
OrugantiSowmya <orugantisowmya19@gmail.com>
2024-07-10 14:11:18 +0530

    Create package.java

---
+++ packages.java
@@ -0,0 +1,21 @@
+package Sowmya;
+class Arth
+{
+void add(int x,int y)
+{
+System.out.println(x+y);
+}
+void sub(int x,int y)
+{
+System.out.println(x-y);
+}
+
+void mul(int x,int y)
+{
+System.out.println(x*y);
+}
+void div(int x,int y)
+{
+System.out.println(x/y);
+}
+}
